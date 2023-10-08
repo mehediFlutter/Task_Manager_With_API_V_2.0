3010eb9e7e4666e87d75b1fa262b29f86d8f8bc5
mehediFluttere <mehedii.edu.bd@gmail.com>
2023-10-08 23:24:39 +0600

    a

---
+++ lib/app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:task_manager/ui/splash_screen.dart';
+import 'package:task_manager/ui/splash_screen.dart';
+
+class TaskManagerApp extends StatelessWidget {
+  const TaskManagerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Task Manager',
+      theme: ThemeData(
+        brightness: Brightness.light,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      themeMode: ThemeMode.light,
+      home: SplashScreen(),
+    );
+  }
+}
