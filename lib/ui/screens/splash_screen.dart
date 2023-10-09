017b3355a14b88278e58f04d40e0ff6bd527a72d
mehediFluttere <mehedii.edu.bd@gmail.com>
2023-10-09 15:44:16 +0600

    Login Screen

--- lib/ui/screens/splash_screen.dart
+++ lib/ui/screens/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:task_manager/ui/utils/assets_utils.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -13,10 +14,10 @@ class SplashScreen extends StatelessWidget {
           SizedBox(
             width: double.infinity,
             height: double.infinity,
-            child: SvgPicture.asset('assets/images/background.svg',fit: BoxFit.fitHeight,),
+            child: SvgPicture.asset(AssetsUtils.backgroundSVG,fit: BoxFit.fitHeight,),
           ),
           Center(
-          child: SvgPicture.asset('assets/images/logo.svg',height:80,width: 100,fit:BoxFit.scaleDown),
+          child: SvgPicture.asset(AssetsUtils.logoSVG,height:80,width: 100,fit:BoxFit.scaleDown),
         ),
         
         ],
